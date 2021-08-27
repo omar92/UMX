4915d5ed80059c6cfcbf160f252451ccf7eea260
oamr sleam <ooomar92@gmail.com>
2021-08-27 02:03:50 +0200

    SOVariables

---
+++ assets/_project/scripts/SO/variables/IntSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SO
+{
+    [CreateAssetMenu(fileName = "IntSO", menuName = "SO/variable/int")]
+    public class IntSO : SOVariable<int>
+    {
+
+    }
+}
---
+++ assets/_project/scripts/SO/variables/SOVariable.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SO
+{
+    public abstract class SOVariable<T> : ScriptableObject
+    {
+       
+        public T Value { get => _value; set { SetValue(value); } }
+        [SerializeField] T startingValue;
+        [SerializeField] private T _value;
+   
+
+        List<Action<T>> subscribers = new List<Action<T>>();
+
+        public void SetValue(T newVal)
+        {
+            if ((_value == null && newVal != null) || (_value != null && !_value.Equals(newVal)))
+            {
+                _value = newVal;
+                for (int i = 0; i < subscribers.Count; i++)
+                {
+                    subscribers[i]?.Invoke(_value);
+                }
+            }
+        }
+
+        public void Subscribe(Action<T> action)
+        {
+            subscribers.Add(action);
+        }
+        public void UnSubscribe(Action<T> action)
+        {
+            subscribers.Remove(action);
+        }
+
+
+        public void OnAfterDeserialize()
+        {
+            _value = startingValue;
+            UnSubscripeAll();
+        }
+        public void OnBeforeSerialize() { UnSubscripeAll(); ResetValue(); }
+        private void UnSubscripeAll()
+        {
+            subscribers.Clear();
+        }
+        private void ResetValue()
+        {
+            _value = startingValue;
+        }
+    }
+}
